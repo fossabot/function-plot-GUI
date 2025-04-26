935b6c50dd96afa4953c49544c8ddf2a7946e836
Linho <lhp@linho.cc>
2025-04-27 01:30:58 +0800

    全局选项框架

--- src/editor/options.vue
+++ src/editor/options.vue
@@ -1,10 +1,79 @@
 <template>
   <s-scroll-view>
-    <s-empty>{{ t('title.inDev') }}</s-empty>
+    <div id="options">
+
+      <s-divider>{{ t("title.global") }}</s-divider>
+
+      <div class="option">
+        <span class="label">{{ t("inputs.grid") }}</span>
+        <s-switch></s-switch>
+      </div>
+      <div class="option">
+        <span class="label">{{ t("inputs.caption") }}</span>
+        <s-text-field :label="t('inputs.caption')"></s-text-field>
+      </div>
+
+      <s-divider>{{ t("title.horizontal") }}</s-divider>
+      <div class="option">
+        <span class="label">{{ t("inputs.grow") }}</span>
+        <s-picker :label="t('inputs.grow')" :key="locale">
+          <s-picker-item value="linear" selected>{{
+            t("inputs.linear")
+          }}</s-picker-item>
+          <s-picker-item value="log">{{ t("inputs.log") }}</s-picker-item>
+        </s-picker>
+      </div>
+      <div class="option">
+        <span class="label">{{ t("inputs.caption") }}</span>
+        <s-text-field :label="t('inputs.caption')"></s-text-field>
+      </div>
+      <div class="option">
+        <span class="label">{{ t("inputs.range") }}</span>
+        <s-text-field :label="t('inputs.caption')"></s-text-field>
+      </div>
+
+      <s-divider>{{ t("title.vertical") }}</s-divider>
+      <div class="option">
+        <span class="label">{{ t("inputs.grow") }}</span>
+        <s-picker :label="t('inputs.grow')" :key="locale">
+          <s-picker-item value="linear" selected>{{
+            t("inputs.linear")
+          }}</s-picker-item>
+          <s-picker-item value="log">{{ t("inputs.log") }}</s-picker-item>
+        </s-picker>
+      </div>
+      <div class="option">
+        <span class="label">{{ t("inputs.caption") }}</span>
+        <s-text-field :label="t('inputs.caption')"></s-text-field>
+      </div>
+      <div class="option">
+        <span class="label">{{ t("inputs.range") }}</span>
+        <s-text-field :label="t('inputs.caption')"></s-text-field>
+      </div>
+    </div>
   </s-scroll-view>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
-const { t } = useI18n();
+const { t ,locale} = useI18n();
 </script>
+
+<style>
+#options {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding-top: 5px;
+}
+#options s-divider {
+  font-size: 0.8em;
+  margin: 5px 15px;
+}
+.option {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 30px;
+}
+</style>
