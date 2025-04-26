70f28608e8e4a6c9eb9be85da5b03dad2d707cca
Linho <lhp@linho.cc>
2025-04-26 23:55:24 +0800

    i18n 初始化提前

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,22 @@
 import type { createI18n } from "vue-i18n";
 type I18nOpt = Parameters<typeof createI18n>[0];
 
+const locale = (() => {
+  if (localStorage) {
+    const localLocale = localStorage.getItem("lang");
+    const langStr = localLocale ?? navigator.language;
+    if (langStr.startsWith("zh")) return "zh-CN";
+    if (langStr.startsWith("en")) return "en-US";
+  }
+  return "en-US";
+})();
+
+if (localStorage && !localStorage.getItem("lang"))
+  localStorage.setItem("lang", locale);
+
 export default {
   legacy: false,
-  locale: "zh-CN",
+  locale,
   messages: {
     "zh-CN": {
       buttons: {
